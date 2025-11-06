118245914f3b8e567c72cd506a7e922197916484
fune6900 <riku.riku1019@icloud.com>
2025-11-06 20:30:57 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ScrollComponent from './ui-component/ScrollComponent/ScrollComponent';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
       {/* Hero Section */}
+      <ScrollComponent />
       <section className="py-16 text-center border-neon-b border-neon-green">
-        <img src="./src/assets/fune.png" 
+        <img src="./public/assets/fune.png" 
           alt="Riku Funagayama Icon"
           className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
         />
+        <img src="./public/assets/ふわちゃん.png" 
+          alt="Riku Funagayama Icon"
+          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
+        />
+        <h1 className="text-4xl font-bold text-neon-white neon-glow-soft">
+          Riku Funagayama
+        </h1>
+        <p className="mt-4 yexy-lg text-neon-white">
+          ただ手を動かすエンジニアではなく、現場と技術の橋渡し役として、課題解決のために自ら考え、行動するエンジニアを目指しています。
+        </p>
       </section>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
         <button className="btn">BUTTONを押してね</button>
       </div>
       <h1 className='bg text-neon-pink neon-glow-soft'>Vite + React</h1>
       <div className="card">
-        <button className="btn" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <label className="field-set font-bold">
           <input type="checkbox" className="neon-checkbox" defaultChecked />
           <span>CHECKBOX</span>
@@ -40,6 +39,46 @@ function App() {
       <p className="read-the-docs ">
         Click on the Vite and React logos to learn more
       </p>
+      <img src="./public/assets/fune.png" 
+          alt="Riku Funagayama Icon"
+          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
+        />
+        <img src="./public/assets/ふわちゃん.png" 
+          alt="Riku Funagayama Icon"
+          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
+        />
+        <img src="./public/assets/fune.png" 
+          alt="Riku Funagayama Icon"
+          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
+        />
+        <img src="./public/assets/ふわちゃん.png" 
+          alt="Riku Funagayama Icon"
+          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
+        />
+        <img src="./public/assets/fune.png" 
+          alt="Riku Funagayama Icon"
+          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
+        />
+        <img src="./public/assets/ふわちゃん.png" 
+          alt="Riku Funagayama Icon"
+          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
+        />
+        <img src="./public/assets/fune.png" 
+          alt="Riku Funagayama Icon"
+          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
+        />
+        <img src="./public/assets/ふわちゃん.png" 
+          alt="Riku Funagayama Icon"
+          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
+        />
+        <img src="./public/assets/fune.png" 
+          alt="Riku Funagayama Icon"
+          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
+        />
+        <img src="./public/assets/ふわちゃん.png" 
+          alt="Riku Funagayama Icon"
+          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
+        />
     </div>
   )
 }
