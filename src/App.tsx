68f087ca4d2744aba4b7b0b7621434f62944bca4
fune6900 <riku.riku1019@icloud.com>
2025-11-07 18:56:52 +0900

    Github pagesに画像が表示されるか確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import ScrollComponent from './ui-component/ScrollComponent/ScrollComponent';
-import './App.css'
+import fune from './assets/fune.png'; 
 
 function App() {
 
@@ -8,13 +8,15 @@ function App() {
       {/* Hero Section */}
       <ScrollComponent />
       <section className="py-16 text-center border-neon-b border-neon-green">
-        <img src="./public/assets/fune.png" 
+        {/* pulic/assets配下*/}
+        <img src="/assets/fune.png" 
           alt="Riku Funagayama Icon"
-          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
+          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pink"
         />
-        <img src="./public/assets/ふわちゃん.png" 
+        {/* src/assets配下*/}
+        <img src={fune} 
           alt="Riku Funagayama Icon"
-          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
+          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pink"
         />
         <h1 className="text-4xl font-bold text-neon-white neon-glow-soft">
           Riku Funagayama
@@ -23,62 +25,6 @@ function App() {
           ただ手を動かすエンジニアではなく、現場と技術の橋渡し役として、課題解決のために自ら考え、行動するエンジニアを目指しています。
         </p>
       </section>
-      <div>
-        <button className="btn">BUTTONを押してね</button>
-      </div>
-      <h1 className='bg text-neon-pink neon-glow-soft'>Vite + React</h1>
-      <div className="card">
-        <label className="field-set font-bold">
-          <input type="checkbox" className="neon-checkbox" defaultChecked />
-          <span>CHECKBOX</span>
-        </label>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs ">
-        Click on the Vite and React logos to learn more
-      </p>
-      <img src="./public/assets/fune.png" 
-          alt="Riku Funagayama Icon"
-          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
-        />
-        <img src="./public/assets/ふわちゃん.png" 
-          alt="Riku Funagayama Icon"
-          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
-        />
-        <img src="./public/assets/fune.png" 
-          alt="Riku Funagayama Icon"
-          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
-        />
-        <img src="./public/assets/ふわちゃん.png" 
-          alt="Riku Funagayama Icon"
-          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
-        />
-        <img src="./public/assets/fune.png" 
-          alt="Riku Funagayama Icon"
-          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
-        />
-        <img src="./public/assets/ふわちゃん.png" 
-          alt="Riku Funagayama Icon"
-          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
-        />
-        <img src="./public/assets/fune.png" 
-          alt="Riku Funagayama Icon"
-          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
-        />
-        <img src="./public/assets/ふわちゃん.png" 
-          alt="Riku Funagayama Icon"
-          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
-        />
-        <img src="./public/assets/fune.png" 
-          alt="Riku Funagayama Icon"
-          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
-        />
-        <img src="./public/assets/ふわちゃん.png" 
-          alt="Riku Funagayama Icon"
-          className="mx-auto w-32 h-32 rounded-full mb-4 border-4 border-neon-pnk"
-        />
     </div>
   )
 }
